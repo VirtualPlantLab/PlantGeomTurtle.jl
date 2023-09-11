4847985e9d2d32553b2f12e00b532eb6a074a15c
Alejandro Morales Sierra <alejandro.moralessierra@wur.nl>
2023-09-11 15:49:21 +0200

    Move O,X,Y,Z to PlantGeomPrimitives

--- docs/make.jl
+++ docs/make.jl
@@ -1,17 +1,11 @@
-using PlantGeomTurtle
-using Documenter
-
-DocMeta.setdocmeta!(PlantGeomTurtle, :DocTestSetup, :(using PlantGeomTurtle); recursive = true)
+using Documenter, PlantGeomTurtle, DocumenterMarkdown
 
 makedocs(;
-    modules = [PlantGeomTurtle],
-    authors = "Alejandro Morales Sierra <alejandro.moralessierra@wur.nl> and contributors",
-    repo = "https://github.com/AleMorales/PlantGeomTurtle.jl/blob/{commit}{path}#{line}",
-    sitename = "PlantGeomTurtle.jl",
-    format = Documenter.HTML(;
-        prettyurls = get(ENV, "CI", "false") == "true",
-        edit_link = "master",
-        assets = String[],
-    ),
-    pages = ["Home" => "index.md"],
-)
+         modules = [PlantGeomTurtle],
+         format = Markdown(),
+         authors = "Alejandro Morales Sierra <alejandro.moralessierra@wur.nl> and contributors",
+         repo = "https://github.com/VirtualPlantLab/PlantGeomTurtle.jl/blob/{commit}{path}#{line}",
+         sitename = "PlantGeomTurtle.jl",
+         pages = [
+             "Home" => "index.md",
+         ])
